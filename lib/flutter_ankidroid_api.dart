93f0cdcf1501d54097e2d79fc59c78480ed45e42
ArtisanLRO <leorafaelorpilla@gmail.com>
2021-04-05 22:52:33 +1000

    Added library file

---
+++ lib/flutter_ankidroid_api.dart
@@ -0,0 +1,35 @@
+library flutter_ankidroid_api;
+
+import 'package:flutter/services.dart';
+
+Future<void> requestPermissions() async {
+  const platform = const MethodChannel('com.lrorpilla.api/ankidroid');
+  await platform.invokeMethod('requestPermissions');
+}
+
+Future<void> addNote(
+  String deck,
+  String image,
+  String audio,
+  String sentence,
+  String answer,
+  String meaning,
+  String reading,
+) async {
+  const platform = const MethodChannel('com.lrorpilla.api/ankidroid');
+
+  try {
+    await platform.invokeMethod('addNote', <String, dynamic>{
+      'deck': deck,
+      'image': image,
+      'audio': audio,
+      'sentence': sentence,
+      'answer': answer,
+      'meaning': meaning,
+      'reading': reading,
+    });
+  } on PlatformException catch (e) {
+    print("Failed to add note via AnkiDroid API");
+    print(e);
+  }
+}
